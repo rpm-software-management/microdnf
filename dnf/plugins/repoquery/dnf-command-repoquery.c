99831d883f2a95f3540a844fe8455f896b9b097d
Jaroslav Rohel <jrohel@redhat.com>
2019-11-10 15:32:31 +0100

    [repoquery] add "--info" and "--nevra" options
    --info   show detailed information about the packages
    --nevra  use name-epoch:version-release.architecture format
             for displaying packages (default)
    
    Signed-off-by: Jaroslav Rohel <jrohel@redhat.com>

--- dnf/plugins/repoquery/dnf-command-repoquery.c
+++ dnf/plugins/repoquery/dnf-command-repoquery.c
@@ -20,6 +20,8 @@
 
 #include "dnf-command-repoquery.h"
 
+#include <libsmartcols.h>
+
 struct _DnfCommandRepoquery
 {
   PeasExtensionBase parent_instance;
@@ -56,6 +58,49 @@ gptrarr_dnf_package_cmp (gconstpointer a, gconstpointer b)
   return dnf_package_cmp(*(DnfPackage**)a, *(DnfPackage**)b);
 }
 
+static void
+package_info_add_line (struct libscols_table *table, const char *key, const char *value)
+{
+  struct libscols_line *ln = scols_table_new_line (table, NULL);
+  scols_line_set_data (ln, 0, key);
+  scols_line_set_data (ln, 1, value);
+}
+
+static void
+print_package_info (DnfPackage *package)
+{
+  struct libscols_table *table = scols_new_table ();
+  scols_table_enable_noheadings (table, 1);
+  scols_table_set_column_separator (table, " : ");
+  scols_table_new_column (table, "key", 5, 0);
+  struct libscols_column *cl = scols_table_new_column (table, "value", 10, SCOLS_FL_WRAP);
+  scols_column_set_safechars (cl, "\n");
+  scols_column_set_wrapfunc (cl, scols_wrapnl_chunksize, scols_wrapnl_nextchunk, NULL);
+
+  package_info_add_line (table, "Name", dnf_package_get_name (package));
+  guint64 epoch = dnf_package_get_epoch (package);
+  if (epoch != 0)
+    {
+      g_autofree gchar *str_epoch = g_strdup_printf ("%ld", epoch);
+      package_info_add_line (table, "Epoch", str_epoch);
+    }
+  package_info_add_line (table, "Version", dnf_package_get_version (package));
+  package_info_add_line (table, "Release", dnf_package_get_release (package));
+  package_info_add_line (table, "Architecture", dnf_package_get_arch (package));
+  g_autofree gchar *size = g_format_size_full (dnf_package_get_size (package),
+                                               G_FORMAT_SIZE_LONG_FORMAT | G_FORMAT_SIZE_IEC_UNITS);
+  package_info_add_line (table, "Size", size);
+  package_info_add_line (table, "Source", dnf_package_get_sourcerpm (package));
+  package_info_add_line (table, "Repository", dnf_package_get_reponame (package));
+  package_info_add_line (table, "Summanry", dnf_package_get_summary (package));
+  package_info_add_line (table, "URL", dnf_package_get_url (package));
+  package_info_add_line (table, "License", dnf_package_get_license (package));
+  package_info_add_line (table, "Description", dnf_package_get_description (package));
+
+  scols_print_table (table);
+  scols_unref_table (table);
+}
+
 static gboolean
 dnf_command_repoquery_run (DnfCommand     *cmd,
                           int              argc,
@@ -65,11 +110,16 @@ dnf_command_repoquery_run (DnfCommand     *cmd,
                           GError         **error)
 {
   gboolean opt_available = FALSE;
+  gboolean opt_info = FALSE;
   gboolean opt_installed = FALSE;
+  gboolean opt_nevra = FALSE;
   g_auto(GStrv) opt_key = NULL;
   const GOptionEntry opts[] = {
     { "available", '\0', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_available, "display available packages (default)", NULL },
+    { "info", '\0', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_info, "show detailed information about the packages", NULL },
     { "installed", '\0', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_installed, "display installed packages", NULL },
+    { "nevra", '\0', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_nevra,
+      "use name-epoch:version-release.architecture format for displaying packages (default)", NULL },
     { G_OPTION_REMAINING, '\0', 0, G_OPTION_ARG_STRING_ARRAY, &opt_key, NULL, NULL },
     { NULL }
   };
@@ -115,16 +165,24 @@ dnf_command_repoquery_run (DnfCommand     *cmd,
 
   g_ptr_array_sort (pkgs, gptrarr_dnf_package_cmp);
 
-  // print packages without duplicated lines
   const char *prev_line = "";
   for (guint i = 0; i < pkgs->len; ++i)
     {
       DnfPackage *package = g_ptr_array_index (pkgs, i);
-      const char * line = dnf_package_get_nevra (package);
-      if (strcmp (line, prev_line) != 0)
+      if (opt_nevra || !opt_info)
         {
-          g_print ("%s\n", line);
-          prev_line = line;
+          const char * line = dnf_package_get_nevra (package);
+          // print nevras without duplicated lines
+          if (opt_info || strcmp (line, prev_line) != 0)
+            {
+              g_print ("%s\n", line);
+              prev_line = line;
+            }
+        }
+      if (opt_info)
+        {
+          print_package_info (package);
+          g_print ("\n");
         }
     }
 
