ac240d755677524c28b52a166ee308f6635e4388
Jaroslav Rohel <jrohel@redhat.com>
2022-03-27 22:18:06 +0200

    Add "swap" command
    Removes package and installs another in one transaction.
    
    = changelog =
    msg:  Adds "swap" command - Removes package and installs another in transaction
    type: enhancement

---
+++ dnf/plugins/swap/dnf-command-swap.h
@@ -0,0 +1,31 @@
+/* dnf-command-swap.h
+ *
+ * Copyright (C) 2022 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "dnf-command.h"
+#include <libpeas/peas.h>
+
+G_BEGIN_DECLS
+
+#define DNF_TYPE_COMMAND_SWAP dnf_command_swap_get_type ()
+G_DECLARE_FINAL_TYPE (DnfCommandSwap, dnf_command_swap, DNF, COMMAND_SWAP, PeasExtensionBase)
+
+G_MODULE_EXPORT void dnf_command_swap_register_types (PeasObjectModule *module);
+
+G_END_DECLS
